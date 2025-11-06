25f3811061b9eb3768da6f2ba63faed1190311ac
Michael Friendly <friendly@yorku.ca>
2025-11-05 20:16:09 -0500

    Initial commit

---
+++ assignments/assign-template.R
@@ -0,0 +1,26 @@
+#' ---
+#' title: "Assignment XX"
+#' author: "YourName"
+#' date: "`r format(Sys.Date())`"
+#' output:
+#'   html_document:
+#'     theme: readable
+#'     code_download: true
+#' ---
+#'
+
+# This sets up some useful options for output
+
+#+ echo=FALSE
+knitr::opts_chunk$set(
+  warning = FALSE,   # avoid warnings and messages in the output
+  message = FALSE
+)
+
+
+#' ## Problem 1
+
+#' Just put your code here, perhaps with comments like this...
+
+#' ### Load libraries ...
+library(vcdExtra)
